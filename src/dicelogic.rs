b4b07db381b7b34d61dec421b47e63d5e839ea3a
hellwaker <samuel_allan_key@live.com.au>
2025-09-18 14:47:40 +0930

    Added basic tests. Code in dicelogic does work right now. Wasn't picked up by compiler before due to dicelogic file not being included in main

--- src/dicelogic.rs
+++ src/dicelogic.rs
@@ -7,17 +7,35 @@ pub fn process_input(String input) -> String {
 }
 
 fn number_node(String input) -> u32 {
-    let result = 1;
-    while input[0] == 0..9 {
+    let i = 0;
+    let result: String = String::new();
+    while input[i] == 0..9 {
         // Grabs the number
+        result.push(input[i]);
+        i++;
     }
-    else if input[0] == "d" {
-        return roll_dice(result, dice_node(input))
+    else if input[i] == "d" {
+        return roll_dice(result, dice_node(input[i..input.length()]))
     }
 }
 
 fn dice_node(String input) -> u32 {
     if input[0] == 0..9 {
         return number_node(input);
+    }
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    //This is testing to get basic functionality working
+    #[test]
+    fn test() {
+        let input = String::from("1d6");
+        let expected = 1;
+        let result = dice_node(input);
+        assert_eq!(expected, result.length());
     }
 }
--- src/roller.rs
+++ src/roller.rs
@@ -1,10 +1,26 @@
-pub fn roll_dice(num_dice : u32, dice_size : u32) -> Vec<u32> 
+pub fn roll_dice(num_dice : u32, mut dice_size : u32) -> Vec<u32> 
 {
     use rand::{rng, Rng};
     let mut rng = rng();
     let mut return_value: Vec<u32> = Vec::new();
+    dice_size +=1;
     for _index in 0..num_dice {
         return_value.push( rng.random_range(1..dice_size));
     }
     return_value
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    //Basic testing seeing if we can force the result to be 1
+    #[test]
+    fn roll() {
+        let result = roll_dice(1, 1);
+        let expected = vec![1];
+        println!("{:?}", result);
+        println!("{:?}", expected);
+        assert_eq!(result,expected);
+    }
 }
